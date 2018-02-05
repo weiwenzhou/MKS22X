afe94caf96c9ed41fefb6bbdd2787b39b762a75d
wzhou2 <wzhou2@stuy.edu>
2018-02-04 19:37:22 -0500

    Wrote factorial using a helper function and tested

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,3 +1,23 @@
 public class Recursion {
-
+    public int fact (int n) {
+        return factHelper(n, 1);
+    }
+    
+    private int factHelper(int n, int product) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 0) {
+            return product;
+        } 
+        return factHelper(n-1, n * product);
+    }
+    
+    public static void main(String[] args) {
+        Recursion r = new Recursion();
+        System.out.println(r.fact(0)); // 1
+        System.out.println(r.fact(6)); // 720
+        System.out.println(r.fact(10)); // 3628800
+        //System.out.println(r.fact(-5)); // exception
+    }
 }
