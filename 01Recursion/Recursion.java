770323635008e0798077513d4cee6ea3d6f62034
wzhou2 <wzhou2@stuy.edu>
2018-02-04 19:50:20 -0500

    Wrote fibonacci using a helper function and tested

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,5 +1,5 @@
 public class Recursion {
-    public int fact (int n) {
+    public int fact(int n) {
         return factHelper(n, 1);
     }
     
@@ -13,11 +13,34 @@ public class Recursion {
         return factHelper(n-1, n * product);
     }
     
+    public int fib(int n) {
+        return fibHelper(n, 0, 1);
+    }
+    
+    private int fibHelper(int n, int a, int b) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 0) {
+            return a;
+        } 
+        return fibHelper(n-1, b, a+b);
+    }
+    
     public static void main(String[] args) {
         Recursion r = new Recursion();
         System.out.println(r.fact(0)); // 1
         System.out.println(r.fact(6)); // 720
         System.out.println(r.fact(10)); // 3628800
         //System.out.println(r.fact(-5)); // exception
+        
+        System.out.println("============");
+        
+        System.out.println(r.fib(0)); // 0
+        System.out.println(r.fib(1)); // 1
+        System.out.println(r.fib(2)); // 1
+        System.out.println(r.fib(5)); // 5
+        System.out.println(r.fib(10)); // 10
+        // System.out.println(r.fib(-5)); // exception
     }
 }
