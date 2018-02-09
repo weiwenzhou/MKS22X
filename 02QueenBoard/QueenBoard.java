9d25c2aed414a249f6486c4a715810383c03737e
wzhou2 <wzhou2@stuy.edu>
2018-02-09 15:21:29 -0500

    Wrote toString

--- 02QueenBoard/QueenBoard.java
+++ 02QueenBoard/QueenBoard.java
@@ -11,26 +11,40 @@ public class QueenBoard {
     }
     
     public String toString() {
-        
+        String returnStr = "";
+        for (int r = 0; r < size; r++) {
+            for (int c = 0; c < size; c++) {
+                int num = board[r][c];
+                if (num == -1) {
+                    returnStr += "Q ";
+                } else {
+                    returnStr += "_ ";
+                }
+            }
+            returnStr += "\n";
+        }
+        return returnStr;
     }
     
     private boolean addQueen(int r, int c) {
-        
+        return true;
     }
     
     private boolean removeQueen(int r, int c) {
-        
+        return true;
     }
     
     public boolean solve() {
-        
+        return true;
     }
     
     public int countSolutions() {
-        
+        return 0;
     }
     
     public static void main(String[] args) {
+        QueenBoard qb = new QueenBoard(5);
         
+        System.out.println(qb);
     }
 }
