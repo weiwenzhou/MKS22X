27018d215f870cd1f95725946c0706840e680c86
wzhou2 <wzhou2@stuy.edu>
2018-02-09 15:15:41 -0500

    Wrote the skeleton of minimum method requires

---
+++ 02QueenBoard/QueenBoard.java
@@ -0,0 +1,31 @@
+public class QueenBoard {
+    private int[][] board;
+
+    public QueenBoard() {
+        
+    }
+    
+    public String toString() {
+        
+    }
+    
+    private boolean addQueen(int r, int c) {
+        
+    }
+    
+    private boolean removeQueen(int r, int c) {
+        
+    }
+    
+    public boolean solve() {
+        
+    }
+    
+    public int countSolutions() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
