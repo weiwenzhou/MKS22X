2166282213b38037ad22f26d732ef204b428f0e6
wzhou2 <wzhou2@stuy.edu>
2018-02-13 18:53:15 -0500

    Took into account of negative perimeters in the constructor and fill in spots with 0s

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,9 +2,26 @@ public class KnightBoard {
     private int[][] board, moveSet;
     
     public KnightBoard(int rows, int cols) {
-        board = new int[rows][cols];
+        try {
+            board = new int[rows][cols];
+        } catch (NegativeArraySizeException e) {
+            throw new IllegalArgumentException();
+        }
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                board[row][col] = 0;
+            }
+        }
         // moveSet = new int[8][2];
         // moveSet[0] = {-1,-2};
         // moveSet = { {-1, -2}, {1, -2}, {-2, -1}, {2, -1}, {-2, 1}, {2, 1}, {-1, 2}, {1, 2} }; 
+    }
+    
+    public boolean solve(int startingRow, int startingCol) {
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        KnightBoard kb = new KnightBoard(2,-1);
     }
 }
