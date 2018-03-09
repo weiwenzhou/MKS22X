417ea732e2101f58a348b744bc1a9c419b88b61b
wzhou2 <wzhou2@stuy.edu>
2018-03-09 12:53:29 -0500

    Finished silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -62,48 +62,65 @@ public class USACO {
         int col = Integer.parseInt(dimensions[1]);
         int time = Integer.parseInt(dimensions[2]);
         char[][] board = new char[row][col];
-	int[][] odd = new int[row][col];
-	int[][] even = new int[row][col];
+        int[][] odd = new int[row][col];
+        int[][] even = new int[row][col];
 
         for (int r = 0; r < row; r++) {
                 String current = in.nextLine();
                 for (int c = 0; c < col; c++) {
                     board[r][c] = current.charAt(c);
-		    odd[r][c] = 0;
-		    even[r][c] = 0;
+                    odd[r][c] = 0;
+                    even[r][c] = 0;
             }
         }
         String[] coords = in.nextLine().split(" ");
-
-	boolean swap = true; // true == even, false == odd
-	
-	for (int step = 0; step < time; step++) {
-	    int sum = 0;
-	    int[][] moveSet = { {0,1}, {0,-1}, {1,0}, {-1,0} };
-
-	    if (swap) {
-		int[][] viewing = odd;
-		int[][] changing = even;
-	    } else {
-
-	    }
-	    
-	    for (int r = 0; r < row; r++) {
-                for (int c = 0; c < col; c++) {
-		    int sum = 0;
-
-		    for (int x = 0; x < 4; x++) {
-			try {
-			    sum += viewing[r+moveSet[x][0]][c+moveSet[x][1]];
-			} catch (IndexOutOfBoundsException e) {}
-		    }
-
-		    changing[r][c] = sum;
-		}
-            }
+        
+        int r1 = Integer.parseInt(coords[0])-1;
+        int c1 = Integer.parseInt(coords[1])-1;
+        int r2 = Integer.parseInt(coords[2])-1;
+        int c2 = Integer.parseInt(coords[3])-1;
+        
+        if (time == 0) {
+            return 0;
+        } else {
+            even[r1][c1] = 1;
         }
+        
+        boolean swap = true; // true == even, false == odd
+	
+        for (int step = 0; step < time; step++) {
+            int[][] moveSet = { {0,1}, {0,-1}, {1,0}, {-1,0} };            
+            for (int r = 0; r < row; r++) {
+                for (int c = 0; c < col; c++) {
+                    if (board[r][c] != '*') {
+                        if (swap) {
+                            if (odd[r][c] == 0) {
+                                int sum = 0;
+                                for (int x = 0; x < 4; x++) {
+                                    try {
+                                        sum += odd[r+moveSet[x][0]][c+moveSet[x][1]];
+                                    } catch (IndexOutOfBoundsException e) {}
+                                }
+                                even[r][c] = sum;
+                            }
+                        } else {
+                            if (even[r][c] == 0) {
+                                int sum = 0;
+                                for (int x = 0; x < 4; x++) {
+                                    try {
+                                        sum += even[r+moveSet[x][0]][c+moveSet[x][1]];
+                                    } catch (IndexOutOfBoundsException e) {}
+                                }
+                                odd[r][c] = sum;
+                            }
+                        }
+                    }
+                }
+            }   
+            swap = !swap;
+        }
+        return even[r2][c2] + odd[r2][c2];
     }
-
     
     public static void main(String[] args) {
         try {
