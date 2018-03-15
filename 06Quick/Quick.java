352db87f432f1177dc4f2ac430dc78333eb71d13
wzhou2 <wzhou2@stuy.edu>
2018-03-15 15:49:29 -0400

    Wrote swap method

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -9,14 +9,36 @@ public class Quick {
                 index++;
             }
         }
-        System.out.println(data[pivot]);
+        data[0] = 100000;
         return index;
+    }
+    
+    public static void swap(int[] data, int start, int end) {
+        int temp = data[end];
+        data[end] = data[start];
+        data[start] = temp;
+    }
+    
+    public static int quickselect(int[] data, int k) {
+        int pivot = (int) (Math.random()*data.length);
+        int temp = data[0];
+        data[0] = data[pivot];
+        data[pivot] = data[0];
+        int smallest = 1;
+        int largest = data.length-1;
+        while (smallest < largest) {
+            if (data[smallest] > data[0]) {
+                
+            }
+        }
+        return -1;
     }
     
     public static void main(String[] args) {
         int[] set = {17, 61, 67, 47, 93, 12, 20, 4, 44, 68};
-        System.out.println(Arrays.toString(set));
+        System.out.println("@1"+Arrays.toString(set));
         System.out.println(partition(set, 0, 4));
+        System.out.println("@2"+Arrays.toString(set));
         System.out.println(partition(set, 1, 6));
     }
 }
