864e391630bd5d923d748c846b3278b53a990982
wzhou2 <wzhou2@stuy.edu>
2018-03-19 10:34:35 -0400

    Changed partition to use dutch flag algorithm

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,23 +1,28 @@
 import java.util.Arrays;
 public class Quick {
-    public static int partition(int[] data, int start, int end) {
+    public static int[] partition(int[] data, int start, int end) {
         int p = (int) (Math.random()*(end-start) + start);
         int pivot = data[p];
-        int low = start+1;
+        int low = start;
         int high = end;
-        swap(data, p, start);
-        while (low <= high) {
-            if (data[low] > pivot) {
-                swap(data, low, high);
-                high--;
-            } else {
+        int i = start;
+        while (i <= high) {
+            if (data[i] == pivot) {
+                i++;
+            } else if (data[i] < pivot) {
+                swap(data, i, low);
                 low++;
+                i++;
+            } else {
+                swap(data, i, high);
+                high--;
             }
         }
         // System.out.println("p"+Arrays.toString(data));
-        swap(data, start, high);
+        // swap(data, start, high);
         // System.out.println("pIndex"+p+",value"+pivot);
-        return high;
+        int[] ary = {low, high};
+        return ary;
     }
     
     public static void swap(int[] data, int start, int end) {
@@ -32,15 +37,15 @@ public class Quick {
     
     public static int quickselect(int[] data, int k, int start, int end) {
         // System.out.println(""+start+","+end+Arrays.toString(data));
-        int index = partition(data, start, end);
+        int[] indexes = partition(data, start, end);
         // System.out.println("part"+Arrays.toString(data));
         // System.out.println("k"+k+",index"+index);
-        if (index == k) {
+        if (indexes[1] == k) {
             return data[k];
-        } else if (index < k) {
-            return quickselect(data, k, index+1, end);
+        } else if (indexes[1] < k) {
+            return quickselect(data, k, indexes[1], end);
         } else {
-            return quickselect(data, k, start, index-1);
+            return quickselect(data, k, start, indexes[0]);
         }
     }
     
@@ -51,10 +56,10 @@ public class Quick {
     public static void quicksort(int[] data, int start, int end) {
         // System.out.println("qstart"+start+"end"+end);
         if (start <= end) {
-            int index = partition(data, start, end);
-            // System.out.println("qIndex"+index+Arrays.toString(data));
-            quicksort(data, start, index-1);
-            quicksort(data, index+1, end);
+            int[] indexes = partition(data, start, end);
+            // System.out.println("qIndex"+indexes[0]+","+indexes[1]+Arrays.toString(data));
+            quicksort(data, start, indexes[0]-1);
+            quicksort(data, indexes[1]+1, end);
         }
     }
     
