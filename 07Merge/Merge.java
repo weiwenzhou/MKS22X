6e6d1a487643a150ca80a31cc6404969c5952a50
wzhou2 <wzhou2@stuy.edu>
2018-03-20 13:49:21 -0400

    Began writing mergesort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,7 +1,23 @@
 import java.util.Arrays;
 public class Merge {
+
     public static void mergesort(int[] data) {
-        int[] replacement = mergesort(data, 0, data.length);
+	int[] temp = new int[data.length];
+	mergesort(data, temp, 0, data.length-1);
+    }
+
+    public static void mergesort(int[] data, int[] temp, int start, int end) {
+	if (start < end) {
+	    mergesort(temp, data, start, (start+end)/2);
+	    mergesort(temp, data, (start+end)/2+1, end);
+	}
+    }
+    
+    // old merge sort.
+    /*
+    public static void mergesort(int[] data) {
+        int[] replacement = mergesort(data, 0, data.length-1);
+	System.out.println(Arrays.toString(replacement));
         for (int x = 0; x < data.length; x++) {
             data[x] = replacement[x];
         }
@@ -13,11 +29,11 @@ public class Merge {
             return ary;
         }
         int total = end-start;
-        int divider = total/2;
+        int divider = total/2+start;
         int[] left = mergesort(data, start, divider);
-        int[] right = mergesort(data, divider+1, start);
+        int[] right = mergesort(data, divider+1, end);
         
-        int[] newAry = new int[total];
+        int[] newAry = new int[total+1];
         int leftIndex = 0;
         int rightIndex = 0;
         int index = 0;
@@ -31,18 +47,22 @@ public class Merge {
                     rightIndex++;
                 }
             } catch (IndexOutOfBoundsException e) {
-                if (leftIndex < rightIndex) {
+                if (leftIndex < left.length) {
                     newAry[index] = left[leftIndex];
                     leftIndex++;
                 } else {
-                    newAry[index] = right[rightIndex];
-                    rightIndex++;
+		    if (rightIndex < right.length) {
+			newAry[index] = right[rightIndex];
+			rightIndex++;
+		    }
                 }
             }
             index++;
         }
         return newAry;
     }
+
+    */
     
     public static void main(String[] args) {
         int[] ary = { 2, 10, 15, 23, 0,  5};
