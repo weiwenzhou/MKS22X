1d30a8575b3d8540ddc2714ad63fbd0060994891
wzhou2 <wzhou2@stuy.edu>
2018-03-26 21:38:37 -0400

    Converted to swapping arrays instead of making new ones

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,69 +1,80 @@
 import java.util.Arrays;
 public class Merge {
-    /*
-    public static void mergesort(int[] data) {
-	int[] temp = new int[data.length];
-	mergesort(data, temp, 0, data.length-1);
-    }
-
-    public static void mergesort(int[] data, int[] temp, int start, int end) {
-	if (start < end) {
-	    mergesort(temp, data, start, (start+end)/2);
-	    mergesort(temp, data, (start+end)/2+1, end);
-	}
-    }
-    */
-    // old merge sort.
     
     public static void mergesort(int[] data) {
-	if (data.length == 0)return;
-        int[] replacement = mergesort(data, 0, data.length-1);
-	//System.out.println(Arrays.toString(replacement));
+        if (data.length == 0)return;
+        int[] replacement = new int[data.length];
+        for (int x = 0; x < data.length; x++) {
+            replacement[x] = data[x];
+        }
+        mergesort(data, replacement, 0, data.length-1);
         for (int x = 0; x < data.length; x++) {
             data[x] = replacement[x];
         }
+        // System.out.println(Arrays.toString(replacement));
     }
     
-    public static int[] mergesort(int[] data, int start, int end) {
-        if (start >= end) {
-            int[] ary = {data[start]};
-            return ary;
+    public static void mergesort(int[] data, int[] temp, int start, int end) {
+        if (end - start <= 5) {
+            // System.out.println("Insertion next"+start+","+end+Arrays.toString(data));
+            insertionsort(data, start, end);
+        } else {
+            if (start < end) {
+                // System.out.println("t"+Arrays.toString(temp));
+                // System.out.println("d"+Arrays.toString(temp));
+                int divider = (start+end)/2;
+                // System.out.println(divider);
+                mergesort(temp, data, start, divider);
+                mergesort(temp, data, divider+1, end);
+                merge(data, temp, start, divider, divider+1, end);
+            }
         }
-        int total = end-start;
-        int divider = total/2+start;
-        int[] left = mergesort(data, start, divider);
-        int[] right = mergesort(data, divider+1, end);
-        
-        int[] newAry = new int[total+1];
-        int leftIndex = 0;
-        int rightIndex = 0;
-        int index = 0;
-        while (index < newAry.length) {
-            try {
-                if (left[leftIndex] < right[rightIndex]) {
-                    newAry[index] = left[leftIndex];
+    }
+
+    public static void merge (int[] data, int[] temp, int lStart, int lEnd, int rStart, int rEnd) {
+        // System.out.println(""+lStart+","+lEnd+":"+rStart+","+rEnd);
+        // System.out.println("t"+Arrays.toString(temp));
+        // System.out.println("d"+Arrays.toString(temp));
+        int leftIndex = lStart;
+        int rightIndex = rStart;
+        int index = lStart;
+        while (index <= rEnd) {
+            if (leftIndex <= lEnd && rightIndex <= rEnd) {
+                if (data[leftIndex] < data[rightIndex]) {
+                    temp[index] = data[leftIndex];
                     leftIndex++;
                 } else {
-                    newAry[index] = right[rightIndex];
+                    temp[index] = data[rightIndex];
                     rightIndex++;
                 }
-            } catch (IndexOutOfBoundsException e) {
-                if (leftIndex < left.length) {
-                    newAry[index] = left[leftIndex];
+            } else {
+                if (leftIndex <= lEnd) {
+                    temp[index] = data[leftIndex];
                     leftIndex++;
                 } else {
-		    if (rightIndex < right.length) {
-			newAry[index] = right[rightIndex];
-			rightIndex++;
-		    }
+                    if (rightIndex <= rEnd) {
+                        temp[index] = data[rightIndex];
+                        rightIndex++;
+                    }
                 }
             }
             index++;
         }
-        return newAry;
     }
-
     
+    public static void insertionsort(int[] data, int start, int end) {
+        for (int x = start; x < end+1; x++) {
+            int temp = data[x];
+            int correctIndex = start;
+            while ( correctIndex < x && temp > data[correctIndex]) {
+                correctIndex++;
+            }
+            for (int i = x; i > correctIndex; i--) {
+                data[i] = data[i-1];
+            }
+            data[correctIndex] = temp;
+        }
+    }
     
     public static void main(String[] args) {
         int[] ary = { 2, 10, 15, 23, 0,  5};
