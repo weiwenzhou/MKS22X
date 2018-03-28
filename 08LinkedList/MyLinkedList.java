f7dfdde05b3e90aa2c262263d5b32cfe7d2d833a
wzhou2 <wzhou2@stuy.edu>
2018-03-28 13:33:49 -0400

    Build skeleton of the class including Node

---
+++ 08LinkedList/MyLinkedList.java
@@ -0,0 +1,65 @@
+public class MyLinkedList {
+    private Node first, last;
+    private int length;
+
+    public MyLinkedList() {
+
+    }
+
+    public boolean add(int value) {
+
+    }
+
+    public int size() {
+	
+    }
+
+    public String toString() {
+
+    }
+
+    public int get(int index) {
+
+    }
+
+    public int set(int index, int newValue) {
+
+    }
+
+    private class Node {
+	private Node next, prev;
+	int data;
+
+	public Node(int value) {
+	    
+	}
+
+	public Node getNext() {
+
+	}
+
+	public Node getPrev() {
+
+	}
+
+	public int getValue() {
+
+	}
+
+	public String toString() {
+
+	}
+
+	public void setNext(Node nextNode) {
+
+	}
+
+	public void setPrev(Node prevNode) {
+
+	}
+
+	public void setValue(int newValue) {
+
+	}
+    }
+}
