20b573d65efd0a6d93df401f9ae220189fcac96c
wzhou2 <wzhou2@stuy.edu>
2018-04-17 10:21:37 -0400

    minor tweak class name and added method for negative values

---
+++ 08LinkedList/Sorts.java
@@ -0,0 +1,39 @@
+public class Sortss{
+    public static void radixsort (MyLinkedListImproved<Integer> data) {
+        @SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] set = new MyLinkedListImproved[19];
+        if (data.size() == 0) {return;}
+        for (int x = 0; x < set.length; x++) {
+            set[x] = new MyLinkedListImproved<Integer>();
+        }
+        int counter = (int) ( Math.log10(data.get(data.max())) )+1;
+        for (int level = 0;level < counter;level++) {
+            while (data.size() != 0) {
+                int index = ((int) ( (data.get(0) % Math.pow(10, level+1)) ))/ ((int)(Math.pow(10,level)));
+                index += 9;
+                set[index].add(data.get(0));
+                data.remove(0);
+            }
+            for (MyLinkedListImproved<Integer> place : set) {
+                data.extend(place);
+            }
+        }
+    }
+    
+    public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
+        radixsort(data)
+    }
+    
+    public static void main (String[] args) {
+        MyLinkedListImproved<Integer> a = new MyLinkedListImproved<>();
+        Integer[] num = {16, 61, 12, 19, 15, 22, 32, -1};
+        
+        for (Integer n : num) {
+            a.add(n);
+        }
+        
+        System.out.println(a);
+        radixsort(a);
+        System.out.println(a);
+        
+    }
+}
