569898bfe77b42775554d9b5a03225e549543e5f
wzhou2 <wzhou2@stuy.edu>
2018-04-19 10:43:41 -0400

    Wrote add methods and toString and resize to test

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -17,5 +17,64 @@ public class MyDeque<T> {
         length = 0;
     }
 
+    public int size() {
+	return length;
+    }
+
+    public void addFirst(T value) {
+	if (start == end) {
+	    resize();
+	}
+	start--;
+	if (start < 0) {
+	    start+= box.length;
+	}
+	box[start] = value;
+	length++;
+    }
+
+    public void addLast(T value) {
+	if (start == end) {
+	    resize();
+	}
+	end++;
+        if (end >= box.length) {
+	    end-= box.length;
+	}
+	box[end] = value;
+	length++;
+    }
+
+    public String toString() {
+	System.out.println("start"+start+"end"+end);
+	String rStr = "[";
+	for (T val : box) {
+	    rStr += val + ",";
+	}
+	return rStr.substring(0, rStr.length()-1) + "]";
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+	T[] newBox = (T[]) new Object[2*box.length];
+	for (int x = 0; x < box.length; x++) {
+	    newBox[x] = box[(start+x)%box.length];
+	}
+	box = newBox;
+	start = 0;
+	end = length;
+    }
     
+    public static void main(String[] args) {
+	MyDeque<Integer> a = new MyDeque<Integer>(2);
+
+	int[] nums = {0,1,2,3,4};
+
+	for (Integer num : nums) {
+	    a.addFirst(num);
+	    System.out.println(a);
+	}
+	a.addFirst(10);
+	System.out.println(a);
+    }
 }
