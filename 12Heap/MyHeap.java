337dcc4436bef225d83db93ba9a967501d35df91
wzhou2 <wzhou2@stuy.edu>
2018-04-29 18:42:28 -0400

    wrote skeleton of class

---
+++ 12Heap/MyHeap.java
@@ -0,0 +1,27 @@
+public class MyHeap {
+    private String[] box;
+    
+    public MyHeap() {
+        
+    }
+    
+    public MyHeap(boolean mode) {
+        
+    }
+    
+    public void add(String s) {
+        
+    }
+    
+    public String remove() {
+        
+    }
+    
+    public String peek() {
+        
+    }
+    
+    public int size() {
+        
+    }
+}
